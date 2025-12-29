1f8f0c689a69191307dd1f2793b0f33d0eadce8f
Victor Mosqueda <vdmc_123@hotmail.com>
2025-12-29 19:25:42 +0000

    changed class to className on footer page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer class="footer">
+    <footer className="footer">
       <p>© 2025 All rights reserved</p>
     </footer>
   );
